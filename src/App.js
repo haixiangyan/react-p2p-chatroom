9b79f683851dfe365b2675a04225d1165a756e9e
haixiangyan <haixiangyan@tencent.com>
2022-02-15 18:14:12 +0800

    feat: 添加消息列表

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import {useEffect, useRef, useState} from "react";
 import Peer from "peerjs";
 
 import styles from './styles.module.scss'
-import {Button, Col, Input, message, Row, Space, Spin} from "antd";
+import {Button, Col, Input, message, Modal, Row, Space, Spin, Switch} from "antd";
+
+const { TextArea } = Input;
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+
   const [localId, setLocalId] = useState('');
   const [remoteId, setRemoteId] = useState('');
+
+  const [messages, setMessages] = useState([]);
+  const [customMsg, setCustomMsg] = useState('');
 
   const currentCall = useRef();
   const currentConnect = useRef();
@@ -40,11 +46,13 @@ const App = () => {
 
     // 纯数据传输
     peer.current.on('connection', (connection) => {
+      // 接受对方传来的数据
       connection.on('data', (data) => {
-        message.info('对方: ' + data);
-        console.log('对方: ' + data);
+        setMessages((curtMessages) => [
+          ...curtMessages,
+          { id: curtMessages.length + 1, type: 'remote', data }
+        ])
       })
-      connection.send('焯！')
 
       currentConnect.current = connection
     })
@@ -82,10 +90,10 @@ const App = () => {
 
     // 数据传输
     const connection = peer.current.connect(remoteId);
-    connection.on('open', () => {
-      connection.send('Hi，我是你爹，收到请务必回复！')
-    })
     currentConnect.current = connection
+    connection.on('open', () => {
+      message.info('已连接')
+    })
 
     // 多媒体传输
     const call = peer.current.call(remoteId, stream)
@@ -103,14 +111,29 @@ const App = () => {
     currentCall.current = call
   }
 
+  const sendMsg = () => {
+    // 发送自定义内容
+    if (!currentConnect.current) {
+      message.warn('还未建立链接')
+    }
+    currentConnect.current.send(customMsg)
+    setMessages((curtMessages) => [
+      ...curtMessages,
+      { id: curtMessages.length + 1, type: 'local', data: customMsg }
+    ])
+  }
+
   return (
     <div className={styles.container}>
       <h1>本地 Peer ID: {localId || <Spin spinning={loading} />}</h1>
-      <Space>
-        <Input value={remoteId} onChange={e => setRemoteId(e.target.value)} type="text" placeholder="对方 Peer 的 Id"/>
-        <Button type="primary" onClick={callUser}>视频通话</Button>
-        <Button type="primary" danger onClick={endCall}>结束通话</Button>
-      </Space>
+
+      <div>
+        <Space>
+          <Input value={remoteId} onChange={e => setRemoteId(e.target.value)} type="text" placeholder="对方 Peer 的 Id"/>
+          <Button type="primary" onClick={callUser}>视频通话</Button>
+          <Button type="primary" danger onClick={endCall}>结束通话</Button>
+        </Space>
+      </div>
 
       <Row gutter={16} className={styles.live}>
         <Col span={12}>
@@ -122,6 +145,23 @@ const App = () => {
           <video controls autoPlay ref={remoteVideo} muted />
         </Col>
       </Row>
+
+      <h1>发送消息</h1>
+      <div>
+        <h2>消息列表</h2>
+        <ul>
+          {messages.map(msg => (
+            <li key={msg.id}>
+              <span>{msg.type === 'local' ? '我' : '对方'}：</span>
+              <span>{msg.data}</span>
+            </li>
+          ))}
+        </ul>
+
+        <h2>自定义消息</h2>
+        <TextArea placeholder="发送自定义内容" value={customMsg} onChange={e => setCustomMsg(e.target.value)} rows={4} />
+        <Button type="primary" onClick={sendMsg}>发送</Button>
+      </div>
     </div>
   );
 }
