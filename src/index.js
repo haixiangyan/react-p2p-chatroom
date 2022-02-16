90489c07442fd3a99560be58ba7752dcb113ec32
haixiangyan <haixiangyan@tencent.com>
2022-02-16 17:31:18 +0800

    feat: 添加 vconsole

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import VConsole from 'vconsole';
 import 'antd/dist/antd.css'
+
+new VConsole();
 
 ReactDOM.render(<App /> , document.getElementById('root'));
